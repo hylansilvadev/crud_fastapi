9704281f69ee2cd832eedda73c612478292b40ed
Hylan Silva <hylansilva@outlook.com>
2024-06-25 00:33:55 -0300

    refactor/async - refatorando API para adicionar o ascincronismo

--- crud_fastapi/database.py
+++ crud_fastapi/database.py
@@ -1,9 +1,17 @@
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 from crud_fastapi.config import settings
 
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# Create a local session class using the async engine
+local_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
--- crud_fastapi/main.py
+++ crud_fastapi/main.py
@@ -1,100 +1,111 @@
 from typing import List
 from fastapi import FastAPI, status, HTTPException
-from sqlmodel import Session, select
+from fastapi.concurrency import asynccontextmanager
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from crud_fastapi.models import User, UserDB, UserView, UserUpdate
 from crud_fastapi.database import create_db_and_tables, engine
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+
+kargs={
+    "title":"Crud FastAPI Playlist",
+    "lifespan":lifespan,
+    "docs_url":"/",
+    "version":"0.7.0"
+}
+
+app = FastAPI(**kargs, )
 
 
-# CREATE 
+# CREATE
+
 
 @app.post(
-    '/users',
+    "/users",
     response_model=UserView,
     status_code=status.HTTP_201_CREATED,
-    response_model_by_alias=False
+    response_model_by_alias=False,
 )
-def create_new_user(user: User):
-    with Session(engine) as session:
+async def create_new_user(user: User):
+    async with AsyncSession(engine) as session:
         user_db = UserDB(**user.model_dump())
         session.add(user_db)
-        session.commit()
-        session.refresh(user_db)
+        await session.commit()
+        await session.refresh(user_db)
         return user_db
+
 
 # READ
 
+
 @app.get(
-    '/users',
+    "/users",
     response_model=List[UserView],
     status_code=status.HTTP_200_OK,
 )
-def get_all_users():
-    with Session(engine) as session:
-        users = session.exec(select(UserDB)).all()
+async def get_all_users():
+    async with AsyncSession(engine) as session:
+        result = await session.exec(select(UserDB))
+        users = result.all()
         return users
 
 
+
 @app.get(
-    '/users/{id}',
+    "/users/{id}",
     response_model=UserView,
     status_code=status.HTTP_200_OK,
-    response_model_by_alias=False
+    response_model_by_alias=False,
 )
-def get_user_by_id(id: int):
-    with Session(engine) as session:
-        user_db = session.get(UserDB, id)
+async def get_user_by_id(id: int):
+    async with AsyncSession(engine) as session:
+        user_db = await session.get(UserDB, id)
         if not user_db:
             raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Usuário não encontrado"
-        )
-        
+                status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
+            )
+
         return user_db
+
+
 # UPDATE
 
-@app.patch(
-    '/users/{id}',
-    response_model=UserView,
-    response_model_by_alias=False
-)
-def update_user_by_id(id: int, user_data: UserUpdate):
-    with Session(engine) as session:
-        user_db = session.get(UserDB, id)
+
+@app.patch("/users/{id}", response_model=UserView, response_model_by_alias=False)
+async def update_user_by_id(id: int, user_data: UserUpdate):
+    async with AsyncSession(engine) as session:
+        user_db = await session.get(UserDB, id)
         if not user_db:
             raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Usuário não encontrado"
-        )
+                status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
+            )
         update_user = user_data.model_dump(exclude_none=True)
         user_db.sqlmodel_update(update_user)
         session.add(user_db)
-        session.commit()
-        session.refresh(user_db)
+        await session.commit()
+        await session.refresh(user_db)
 
         return user_db
 
-# DELETE 
 
-@app.delete(
-    '/users/{id}',
-    status_code=status.HTTP_204_NO_CONTENT
-)
-def delete_user_by_id(id: int):
-    with Session(engine) as session:
-        user_db = session.get(UserDB, id)
+# DELETE
+
+
+@app.delete("/users/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user_by_id(id: int):
+    async with AsyncSession(engine) as session:
+        user_db = await session.get(UserDB, id)
         if not user_db:
             raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Usuário não encontrado"
-        )
-        
-        session.delete(user_db)
-        session.commit()
+                status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
+            )
+
+        await session.delete(user_db)
+        await session.commit()
